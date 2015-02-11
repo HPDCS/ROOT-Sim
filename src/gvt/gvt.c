4a9899a8b67163540e3d921fb48b3b44b3897fa7
Alessandro Pellegrini <alessandro@pellegrini.tk>
2015-02-11 12:47:42 +0100

    Fixing a bug in GVT
    When running with ULTs, creating LPs stacks might take time. In this case, the assumption on bound being != NULL fails, and minimization of the GVT produces a segmentation fault. This commit adds a sanity check on this

--- src/gvt/gvt.c
+++ src/gvt/gvt.c
@@ -37,6 +37,7 @@
 #include <statistics/statistics.h>
 
 
+static bool first_gvt_invocation = true;
 
 // Defintion of GVT-reduction phases
 enum gvt_phases {phase_A, phase_send, phase_B, phase_aware, phase_end};
@@ -173,6 +174,17 @@ simtime_t gvt_operations(void) {
 	// This is different from the paper's pseudocode to reduce
 	// slightly the number of clock reads
 	if(GVT_flag == 0 && atomic_read(&counter_end) == 0) {
+		
+		
+		// When using ULT, creating stacks might require more time than
+		// the first gvt phase. In this case, we enter the GVT reduction
+		// before running INIT. This makes all the assumptions about the
+		// fact that bound is not null fail, and everything here inevitably
+		// crashes. This is a sanity check for this.
+		if(first_gvt_invocation) {
+			first_gvt_invocation = false;
+			timer_restart(gvt_timer);
+		}
 
 		// Has enough time passed since the last GVT reduction?
 		if ( timer_value_milli(gvt_timer) > (int)rootsim_config.gvt_time_period &&
