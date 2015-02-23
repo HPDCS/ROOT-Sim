73a86ba14972f105f888fa715a738ffab2b3b1ed
Alessandro Pellegrini <alessandro@pellegrini.tk>
2015-02-23 13:52:01 +0100

    Non-blocking load sharing algorithm

--- src/scheduler/binding.c
+++ src/scheduler/binding.c
@@ -27,6 +27,7 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include <arch/atomic.h>
 #include <core/core.h>
 #include <core/timer.h>
 #include <datatypes/list.h>
@@ -36,10 +37,15 @@
 #include <statistics/statistics.h>
 
 
+#define REBIND_INTERVAL 10.0
+
+
 struct lp_cost_id {
 	double workload_factor;
 	unsigned int id;
 };
+
+struct lp_cost_id *lp_cost;
 
 
 
@@ -51,9 +57,17 @@ static __thread bool first_lp_binding = true;
  */
 __thread LP_state **LPS_bound = NULL;
 
+static timer rebinding_timer;
 
-timer rebinding_timer;
+static unsigned int *new_LPS_binding;
 
+static int binding_phase = 0;
+static __thread int local_binding_phase = 0;
+
+static int binding_acquire_phase = 0;
+static __thread int local_binding_acquire_phase = 0;
+
+static atomic_t worker_thread_reduction;
 
 
 /**
@@ -137,27 +151,13 @@ static inline void LP_knapsack(void) {
 	bool assigned;
 	double assignments[n_cores];
 
-	// This must become global
-	unsigned int new_LPS_bound[n_prc];
-
-	struct lp_cost_id lp_cost[n_prc];
-
 	if(!master_thread())
 		return;
 
-	// Estimate the value
-//	reference_lvt = get_last_gvt() + statistics_get_data(STAT_GET_SIMTIME_ADVANCEMENT, 0.0);
-
-	// Estimate the costs
+	// Estimate the reference knapsack
 	for(i = 0; i < n_prc; i++) {
-		lp_cost[i].id = i;
-		lp_cost[i].workload_factor = list_sizeof(LPS[i]->queue_in);
-		lp_cost[i].workload_factor *= statistics_get_data(STAT_GET_EVENT_TIME_LP, i);
-		lp_cost[i].workload_factor /= ( list_tail(LPS[i]->queue_in)->timestamp - list_head(LPS[i]->queue_in)->timestamp );
 		reference_knapsack += lp_cost[i].workload_factor;
-		printf("LP %d has an estimated %f workload factor\n", i, lp_cost[i].workload_factor);
 	}
-
 	reference_knapsack /= n_cores;
 
 	printf("max is %d\n", reference_knapsack);
@@ -165,21 +165,15 @@ static inline void LP_knapsack(void) {
 	// Sort the expected times
 	qsort(&lp_cost, n_prc, sizeof(struct lp_cost_id) , compare_lp_cost);
 
-	printf("Sorting is: ");
-	for(i = 0; i < n_prc; i++)
-		printf("%d ", lp_cost[i].id);
-	puts("");
-
 	// At least one LP per thread
 	bzero(assignments, sizeof(double) * n_cores);
 	j = 0;
 	for(i = 0; i < n_cores; i++) {
 		assignments[j] += lp_cost[i].workload_factor;
-		new_LPS_bound[i] = j;
+		new_LPS_binding[i] = j;
 		j++;
 	}
 	
-
 	// Very suboptimal approximation of knapsack
 	for(; i < n_prc; i++) {
 		assigned = false;
@@ -188,7 +182,7 @@ static inline void LP_knapsack(void) {
 			// Simulate assignment
 			if(assignments[j] + lp_cost[i].workload_factor <= reference_knapsack) {
 				assignments[j] += lp_cost[i].workload_factor;
-				new_LPS_bound[i] = j;
+				new_LPS_binding[i] = j;
 				printf("LP %d goes to thread %d\n", i, j);
 				assigned = true;
 				break;
@@ -204,7 +198,7 @@ static inline void LP_knapsack(void) {
 		printf("I have %d leftovers\n", n_prc - i);
 		j = 0;
 		for( ; i < n_prc; i++) {
-			new_LPS_bound[i] = j;
+			new_LPS_binding[i] = j;
 			printf("Force assigning LP %d to thread %d\n", i, j);
 			j = (j + 1) % n_cores;
 		}
@@ -214,7 +208,7 @@ static inline void LP_knapsack(void) {
 	for(j = 0; j < n_cores; j++) {
 		printf("Thread %d: ", j);
 		for(i = 0; i < n_prc; i++) {
-			if(new_LPS_bound[i] == j)
+			if(new_LPS_binding[i] == j)
 				printf("%d ", i);
 		}
 		printf("\n");
@@ -224,6 +218,38 @@ static inline void LP_knapsack(void) {
 	//~ LPS_bound[n_prc_per_thread++] = LPS[i];
 	//~ LPS[i]->worker_thread = tid;
 
+}
+
+
+static void post_local_reduction(void) {
+	unsigned int i;
+	unsigned int lid;
+
+	for(i = 0; i < n_prc_per_thread; i++) {
+		lid = LPS_bound[i]->lid;
+
+		lp_cost[lid].id = i;
+		lp_cost[lid].workload_factor = list_sizeof(LPS[lid]->queue_in);
+		lp_cost[lid].workload_factor *= statistics_get_data(STAT_GET_EVENT_TIME_LP, lid);
+		lp_cost[lid].workload_factor /= ( list_tail(LPS[lid]->queue_in)->timestamp - list_head(LPS[lid]->queue_in)->timestamp );
+		printf("LP %d has an estimated %f workload factor\n", lid, lp_cost[lid].workload_factor);
+	}
+}
+
+
+static void install_binding(void) {
+	unsigned int i;
+
+	bzero(LPS_bound, sizeof(LP_state *) * n_prc);
+	n_prc_per_thread = 0;
+
+	for(i = 0; i < n_prc; i++) {
+		if(new_LPS_binding[i] == tid) {
+			LPS_bound[n_prc_per_thread++] = LPS[i];
+			// TODO: NUMA MIgration API here
+			LPS[i]->worker_thread = tid;
+		}
+	}
 }
 
 
@@ -251,11 +277,39 @@ void rebind_LPs(void) {
 
 		timer_start(rebinding_timer);
 
+		if(master_thread()) {
+			new_LPS_binding = rsalloc(sizeof(int) * n_prc);
+			lp_cost = rsalloc(sizeof(struct lp_cost_id) * n_prc);
+
+			atomic_set(&worker_thread_reduction, n_cores);
+		}
+
 		return;
 	}
 
-	if(master_thread() && timer_value_seconds(rebinding_timer) >= 10.0) {
-		timer_restart(rebinding_timer);
-		LP_knapsack();
+	if(master_thread()) {
+		if(timer_value_seconds(rebinding_timer) >= REBIND_INTERVAL) {
+			timer_restart(rebinding_timer);
+			binding_phase++;
+		} else if(atomic_read(&worker_thread_reduction) == 0) {
+			LP_knapsack();
+			binding_acquire_phase++;
+		}
+	}
+
+	if(local_binding_phase < binding_phase) {
+		local_binding_phase = binding_phase;
+		post_local_reduction();
+		atomic_dec(&worker_thread_reduction);
+	}
+
+	if(local_binding_acquire_phase < binding_acquire_phase) {
+		local_binding_acquire_phase = binding_acquire_phase;
+		install_binding();
+		if(thread_barrier(&all_thread_barrier)) {
+			atomic_set(&worker_thread_reduction, n_cores);
+		}
 	}
 }
+
+
