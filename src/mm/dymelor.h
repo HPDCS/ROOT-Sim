d64dbbdba5ea3edc954536b834c1d47c3c779ec7
Alessandro Pellegrini <pellegrini@dis.uniroma1.it>
2015-10-30 18:58:02 +0100

    Introduce slab allocator
    This slab allocator can be used on any data type, but it will be
    used for now to quickly serve requests of revwins
    
    Signed-off-by: Alessandro Pellegrini <pellegrini@dis.uniroma1.it>

--- src/mm/dymelor.h
+++ src/mm/dymelor.h
@@ -122,6 +122,9 @@
 #define RESET_BIT_AT(B,K) ( B &= ~(MASK << K) )
 #define CHECK_BIT_AT(B,K) ( B & (MASK << K) )
 
+#define POWEROF2(x) (1UL << (1 + (63 - __builtin_clzl((x) - 1))))
+#define IS_POWEROF2(x) ((x) != 0 && ((x) & ((x) - 1)) == 0)
+
 
 /// This structure let DyMeLoR handle one malloc area (for serving given-size memory requests)
 struct _malloc_area {
