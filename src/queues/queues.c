844d2a491cd4814acd83fa3e8e91a64130f21372
ael-code <tommy.ael@gmail.com>
2017-08-18 15:58:34 +0200

    fix line indentation

--- src/queues/queues.c
+++ src/queues/queues.c
@@ -282,7 +282,7 @@ restart:
 						LPS[lid_receiver]->state = LP_STATE_ROLLBACK;
 					}
 #ifdef HAS_MPI
-						register_incoming_msg(msg_to_process);
+					register_incoming_msg(msg_to_process);
 #endif
 					break;
 
