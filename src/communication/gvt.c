05205bfc120df8e408d7237280f430af0286f536
Alessandro Pellegrini <pellegrini@dis.uniroma1.it>
2018-12-12 08:56:34 +0100

    Better documentation of MPI/GVT and higher code coverage
    This commit documents more data structures and functions in
    communication/gvt.c.
    The test suite has been reorganized so as to make use of the
    run tests in the code coverage.
    
    Signed-off-by: Alessandro Pellegrini <pellegrini@dis.uniroma1.it>

--- src/communication/gvt.c
+++ src/communication/gvt.c
@@ -3,7 +3,16 @@
 *
 * @brief Distributed GVT Support module
 *
-* Distributed GVT Support module
+* This module implements communication primitives to implement the
+* asynchronous distributed GVT reduction algorithm described in:
+*
+* T. Tocci, A. Pellegrini, F. Quaglia, J. Casanovas-García, and T. Suzumura,</br>
+* <em>“ORCHESTRA: An Asynchronous Wait-Free Distributed GVT Algorithm,”</em></br>
+* in Proceedings of the 21st IEEE/ACM International Symposium on Distributed
+* Simulation and Real Time Applications</br>
+* 2017
+*
+* For a full understanding of the code, we encourage reading that paper.
 *
 * @copyright
 * Copyright (C) 2008-2018 HPDCS Group
@@ -32,47 +41,136 @@
 #include <communication/mpi.h>
 #include <communication/gvt.h>
 
-/// current colour phase of each thread
+/**
+ * Colour phase of each thread. This is a dynamically sized array
+ * which tells the current execution phase of each thread (white or red).
+ */
 phase_colour *threads_phase_colour;
 
-/// minimum time among all the outgoing red messages for each thread
+/// Minimum time among all the outgoing red messages for each thread
 simtime_t *min_outgoing_red_msg;
 
 /**
- * `white_msg_recv` must always point to the
+ * There are multiple incarnations of a white phase, namely the white
+ * phase @e before a red phase is different from the white phase @e after
+ * that same red phase. At the end, we can reduce the multiple incarnations
+ * to only two (the third white phase appearing in the execution can
+ * be merged with the first, as the second one in between ensures that
+ * no pending action on the first phase is present).
+ *
+ * This aspect is implemented using two different atomic counters of
+ * white messages, so that @ref white_msg_recv must always point to the
  * actual counter of white message received by the current kernel
  * that have been sent during the last global white phase.
- * It will point either to `white_0_msg_recv` or
- * to `white_1_msg_recv`.
+ * It will point either to @ref white_0_msg_recv or to @ref white_1_msg_recv.
  */
 volatile atomic_t *white_msg_recv;
-atomic_t white_0_msg_recv;
-atomic_t white_1_msg_recv;
 
-/** number of white message sent from this kernel
-   to all the others during the last white phase */
-atomic_t *white_msg_sent;
+/**
+ * Counter of received white messages for the first white phase in a period
+ * of three. This is pointed to by @ref white_msg_recv when relating to
+ * the current white phase.
+ */
+static atomic_t white_0_msg_recv;
 
-/// temporary structure used for the MPI collective
+/**
+ * Counter of received white messages for the second white phase in a period
+ * of three. This is pointed to by @ref white_msg_recv when relating to
+ * the current white phase.
+ */
+static atomic_t white_1_msg_recv;
+
+/**
+ * Number of white message sent from this kernel to all the others
+ * during the last white phase
+ */
+static atomic_t *white_msg_sent;
+
+/// Temporary structure used for the MPI collective primitives
 static int *white_msg_sent_buff;
 
-/** number of expected white message to be received by this kernel
-   before to partecipate to the next GVT agreement */
+/**
+ * Number of expected white message to be received by this kernel
+ * before partecipating to the next GVT reduction
+ */
 static int expected_white_msg;
-MPI_Request white_count_req;
-MPI_Comm white_count_comm;
-spinlock_t white_count_lock;
-bool white_count_pending;
 
-MPI_Request gvt_reduction_req;
-MPI_Comm gvt_reduction_comm;
-spinlock_t gvt_reduction_lock;
-simtime_t local_vt_buff;
-simtime_t reduced_gvt;
+/**
+ * MPI Request operation to implement the asynchronous counting
+ * of white messages
+ */
+static MPI_Request white_count_req;
 
-MPI_Request *gvt_init_reqs;
-unsigned int gvt_init_round;
+/**
+ * Separate MPI communicator to carry out the reduction of white
+ * messages. This communicator lives for the whole duration of the
+ * simulation.
+ */
+static MPI_Comm white_count_comm;
 
+/**
+ * Spinlock guard used to ensure that only one thread at a time
+ * attempts to perform check operations on the reduction of white
+ * messages.
+ */
+static spinlock_t white_count_lock;
+
+/**
+ * MPI Request operation to implement the distributed reduction
+ * of the current GVT value.
+ */
+static MPI_Request gvt_reduction_req;
+
+/**
+ * Separate MPI communicator to carry out the reduction of the GVT.
+ */
+static MPI_Comm gvt_reduction_comm;
+
+/**
+ * Spinlock guard used to ensure that only one thread at a time
+ * attempts to perform check operations on the reduction of the GVT.
+ */
+static spinlock_t gvt_reduction_lock;
+
+/**
+ * In the end, the GVT reduction is implemented using an MPI All Reduce.
+ * This primitive requires some stable buffer in memory to keep the
+ * values to be reduced across all ranks. This is a global variable
+ * in which each kernel instance places its proposal for the GVT, which
+ * is reduced using MPI all reduce.
+ */
+static simtime_t local_vt_buff;
+
+/**
+ * This is the target temporary buffer in which MPI all reduce will
+ * place the reduced GVT value.
+ */
+static simtime_t reduced_gvt;
+
+/**
+ * A vector of MPI asynchronous operations used to communicate with all
+ * kernel instances in the distributed run in order to initiate the GVT
+ * reduction protocol.
+ */
+static MPI_Request *gvt_init_reqs;
+
+/**
+ * This variable is used to keep track of what round of GVT reduction we
+ * are actually starting. This is used also to make a sanity check on
+ * whether we are starting a new GVT reduction while the previous one
+ * is still pending.
+ */
+static unsigned int gvt_init_round;
+
+
+/**
+ * @brief Initialize the MPI-based distributed GVT reduction submodule.
+ *
+ * This function is called after that the MPI subsystem is activated.
+ * Its goal is to initialize all the variable-sized data structures
+ * and MPI-related data structures which will be used while running the
+ * distributed GVT reduction protocol.
+ */
 void gvt_comm_init(void)
 {
 	unsigned int i;
@@ -110,6 +208,16 @@ void gvt_comm_init(void)
 	spinlock_init(&gvt_reduction_lock);
 }
 
+
+/**
+ * @brief Shut down the MPI-based distributed GVT reduction submodule.
+ *
+ * This function is called just before shutting down the MPI subsystem.
+ * It frees all the data structures used to carry out the distributed GVT
+ * reduction protocol, and nicely tells MPI to wipe out everything that
+ * is no longer needed.
+ * Right after this, MPI will be shut down as well.
+ */
 void gvt_comm_finalize(void)
 {
 	rsfree(min_outgoing_red_msg);
@@ -136,18 +244,21 @@ void gvt_comm_finalize(void)
 	rsfree(gvt_init_reqs);
 }
 
+
 /**
- * Make a thread enter into the red phase.
+ * @brief Make a thread enter into red phase.
  *
- * The calling thread phase will be turn to red and
- * its minimum timestamp among the sent red messages will be reset.
+ * Once this function returns, the calling thread will be turned @b red.
+ * The minimum timestamp of red messages sent by it will be reset.
  *
- * An error will be raised in the case the calling thread is already into
- * a red phase.
+ * @warning Calling this function from a thread which is already in the
+ *          red phase will cause a sanity check to immediately stop the
+ *          simulation, as it will be no longer possible to ensure
+ *          consistency in the GVT reduction.
  */
 void enter_red_phase(void)
 {
-	if (in_red_phase()) {
+	if (unlikely(in_red_phase())) {
 		rootsim_error(true, "Thread %u cannot enter in red phase because is already in red phase.\n", local_tid);
 	}
 	min_outgoing_red_msg[local_tid] = INFTY;
@@ -155,22 +266,25 @@ void enter_red_phase(void)
 }
 
 /**
- * Make a thread exit from the red phase.
+ * @brief Make a thread exit from red phase.
  *
- * The calling thread phase will be turn to white.
+ * Once this function returns, the calling thread will be turned @b white.
  *
- * An error will be raised in the case the calling thread is not in red phase.
+ * @warning Calling this function from a thread which is already in the
+ *          white phase will cause a sanity check to immediately stop the
+ *          simulation, as it will be no longer possible to ensure
+ *          consistency in the GVT reduction.
  */
 void exit_red_phase(void)
 {
-	if (!in_red_phase()) {
+	if (unlikely(!in_red_phase())) {
 		rootsim_error(true, "Thread %u cannot exit from red phase because it wasn't in red phase.\n", local_tid);
 	}
 	threads_phase_colour[local_tid] = next_colour(threads_phase_colour[local_tid]);
 }
 
 /**
- * Join the white message reduction collective operation.
+ * @brief Join the white message reduction collective operation.
  *
  * All kernels will share eachother the number of white message sent
  * during the last white phase.
@@ -210,7 +324,8 @@ bool white_msg_redux_completed(void)
 }
 
 /**
- * Syncronously wait for the completion of white message reduction collective operation.
+ * @brief Syncronously wait for the completion of white message reduction
+ *        collective operation.
  */
 void wait_white_msg_redux(void)
 {
@@ -218,6 +333,8 @@ void wait_white_msg_redux(void)
 }
 
 /**
+ * @brief Check if white messages are all received
+ * 
  * Check if the number of white message received is equal to
  * the expected ones (retrieved through the white message reduction).
  */
@@ -274,8 +391,7 @@ void broadcast_gvt_init(unsigned int round)
 			rootsim_error(true, "Failed to send new GVT init round to kernel %u, because the old init request is still pending\n", i);
 		}
 		lock_mpi();
-		MPI_Isend((const void *)&gvt_init_round, 1, MPI_UNSIGNED, i,
-			  MSG_NEW_GVT, MPI_COMM_WORLD, &gvt_init_reqs[i]);
+		MPI_Isend((const void *)&gvt_init_round, 1, MPI_UNSIGNED, i, MSG_NEW_GVT, MPI_COMM_WORLD, &gvt_init_reqs[i]);
 		unlock_mpi();
 	}
 }
