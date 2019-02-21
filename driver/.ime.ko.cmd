a8dc91a96137d60ec5591829df6e8b7d5e43c6ef
Stefano Carna <stefano.carna.dev@gmail.com>
2019-02-21 14:45:12 +0100

    Fixed Intel event and added global msr
    Signed-off-by: Serena Ferracci <serenaferracci95@gmail.com>
    Signed-off-by: Stefano Carnà <carna@diag.uniroma1.it>
    Signed-off-by: Alessandro Pellegrini <pellegrini@dis.uniroma1.it>

---
+++ driver/.ime.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/serena/Scrivania/IME/driver/ime.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/serena/Scrivania/IME/driver/ime.ko /home/serena/Scrivania/IME/driver/ime.o /home/serena/Scrivania/IME/driver/ime.mod.o ;  true
---
+++ driver/.tmp_versions/ime.mod
@@ -0,0 +1,3 @@
+/home/serena/Scrivania/IME/driver/ime.ko
+/home/serena/Scrivania/IME/driver/ime_mod.o /home/serena/Scrivania/IME/driver/control.o /home/serena/Scrivania/IME/driver/irq_facility.o
+
