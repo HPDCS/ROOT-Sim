d7d593ccce895f477383f9bb61963e68e6c870d1
serenaferracci <serenaferracci95@gmail.com>
2019-03-23 18:52:46 +0100

    Added module parameters
    Signed-off-by: Serena Ferracci <serenaferracci95@gmail.com>
    Signed-off-by: Stefano Carnà <carna@diag.uniroma1.it>
    Signed-off-by: Alessandro Pellegrini <pellegrini@dis.uniroma1.it>

--- joejoe/driver/patcher.h
+++ joejoe/driver/patcher.h
@@ -4,7 +4,7 @@
 #define SWITCH_FUNC "finish_task_switch"
 #define MSR_IA32_PERF_GLOBAL_CTRL	    0x38F
 
-typedef void h_func(void *);
+typedef void h_func(void);
 
 int switch_patcher_init (unsigned long func_addr);
 void switch_patcher_exit(void);
