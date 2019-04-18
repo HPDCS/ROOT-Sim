bf87afd85809e4a04c7f9bb320340fcc913a24e6
Alessandro Pellegrini <pellegrini@dis.uniroma1.it>
2019-04-18 18:02:20 +0200

    Mark chunks as written only if running with ISS
    We check in wrappers whether we are configured to run using ISS. If this
    is not the case, we don't mark the dirty bitmap.
    
    Signed-off-by: Alessandro Pellegrini <pellegrini@dis.uniroma1.it>

--- src/mm/wrapper.c
+++ src/mm/wrapper.c
@@ -29,8 +29,7 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <core/timer.h>
-#include <scheduler/scheduler.h>
+#include <core/init.h>
 #include <mm/dymelor.h>
 
 // Definitions to functions which will be wrapped by the linker
@@ -45,43 +44,50 @@ void *__real_memset(void *, int, size_t);
 // Actual wrappers
 __visible char *__wrap_strcpy(char *s, const char *ct)
 {
-	__write_mem(s, -1);
+	if(rootsim_config.snapshot == SNAPSHOT_INCREMENTAL)
+		__write_mem((unsigned char *)s, -1);
 	return __real_strcpy(s, ct);
 }
 
 __visible char *__wrap_strncpy(char *s, const char *ct, size_t n)
 {
-	__write_mem((void *)s, n);
+	if(rootsim_config.snapshot == SNAPSHOT_INCREMENTAL)
+		__write_mem((unsigned char *)s, n);
 	return __real_strncpy(s, ct, n);
 }
 
 __visible char *__wrap_strcat(char *s, const char *ct)
 {
-	__write_mem(s, -1);
+	if(rootsim_config.snapshot == SNAPSHOT_INCREMENTAL)
+		__write_mem((unsigned char *)s, -1);
 	return __real_strcat(s, ct);
 }
 
 __visible char *__wrap_strncat(char *s, const char *ct, size_t n)
 {
-	__write_mem(s, n);
+	if(rootsim_config.snapshot == SNAPSHOT_INCREMENTAL)
+		__write_mem((unsigned char *)s, n);
 	return __real_strncat(s, ct, n);
 }
 
 __visible void *__wrap_memcpy(void *s, const void *ct, size_t n)
 {
-	__write_mem(s, n);
+	if(rootsim_config.snapshot == SNAPSHOT_INCREMENTAL)
+		__write_mem((unsigned char *)s, n);
 	return __real_memcpy(s, ct, n);
 }
 
 __visible void *__wrap_memmove(void *s, const void *ct, size_t n)
 {
-	__write_mem(s, n);
+	if(rootsim_config.snapshot == SNAPSHOT_INCREMENTAL)
+		__write_mem((unsigned char *)s, n);
 	return __real_memmove(s, ct, n);
 }
 
 __visible void *__wrap_memset(void *s, int c, size_t n)
 {
-	__write_mem(s, n);
+	if(rootsim_config.snapshot == SNAPSHOT_INCREMENTAL)
+		__write_mem((unsigned char *)s, n);
 	return __real_memset(s, c, n);
 }
 
@@ -89,7 +95,8 @@ __visible char *__wrap_strdup(const char *s)
 {
 	char *ret = (char *)__wrap_malloc(strlen(s) + 1);
 	__real_strcpy(ret, s);
-	__write_mem(ret, strlen(s));
+	if(rootsim_config.snapshot == SNAPSHOT_INCREMENTAL)
+		__write_mem((unsigned char *)ret, strlen(s));
 	return ret;
 }
 
@@ -97,6 +104,7 @@ __visible char *__wrap_strndup(const char *s, size_t n)
 {
 	char *ret = (char *)__wrap_malloc(n);
 	__real_strncpy(ret, s, n);
-	__write_mem(ret, n);
+	if(rootsim_config.snapshot == SNAPSHOT_INCREMENTAL)
+		__write_mem((unsigned char *)ret, n);
 	return ret;
 }
