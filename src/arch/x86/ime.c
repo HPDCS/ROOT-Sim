2fe9b34dc7634dcaf4bca7f17aad814a48b348b9
Alessandro Pellegrini <pellegrini@dis.uniroma1.it>
2019-04-18 10:10:03 +0200

    Implementing of the IME userspace interface
    This commit introduces a userspace interface to toggle the data access
    traacking offered by the IME module.
    
    Signed-off-by: Alessandro Pellegrini <pellegrini@dis.uniroma1.it>

---
+++ src/arch/x86/ime.c
@@ -0,0 +1,8 @@
+#ifdef HAVE_PMU
+
+#include <arch/x86/ime.h>
+
+uint32_t pmu_msr_high = PMU_MSR_HIGH_VALUE;
+uint32_t pmu_msr_low = 0;
+
+#endif /* HAVE_PMU */
---
+++ src/arch/x86/ime.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifdef HAVE_PMU
+
+#include <stdint.h>
+
+#define PMU_MSR_HIGH_VALUE	0x0
+#define PMU_MSR_LOW_VALUE	0xf
+
+#define PMU_MSR_CODE		0x38f
+
+extern uint32_t pmu_msr_high;
+extern uint32_t pmu_msr_low;
+
+static inline void __toggle_pmu_trace(unsigned int msr, uint32_t low, uint32_t high)
+{
+	__asm__ __volatile__("int $0xf0\n" // Linux reserved for future use IRQ vector
+		     : : "c" (msr), "a"(low), "d" (high) : "memory");
+}
+
+static inline void toggle_pmu_trace(void) {
+	pmu_msr_low ^= PMU_MSR_LOW_VALUE;
+	__toggle_pmu_trace(PMU_MSR_CODE, pmu_msr_low, pmu_msr_high);
+}
+
+
+#endif /* HAVE_PMU */
