79dafcb5e021231201428454acab8950b8bd5bfd
Piccions <piccions@rocketmail.com>
2020-01-27 20:34:38 +0100

    Small fix on the build without approximated rollback support
    Criticism and suggestions are always welcome!
    
    Signed-off-by: Andrea Piccione <piccions@gmx.com>

--- src/mm/checkpoints.c
+++ src/mm/checkpoints.c
@@ -391,8 +391,10 @@ void log_restore(struct lp_struct *lp, state_t *state_queue_node)
 {
 	statistics_post_data(lp, STAT_RECOVERY, 1.0);
 	restore_full(lp, state_queue_node->log);
+#ifdef HAVE_APPROXIMATED_ROLLBACK
 	if(lp->mm->m_state->is_approximated)
 		RestoreApproximated(lp->current_base_pointer);
+#endif
 }
 
 /**
