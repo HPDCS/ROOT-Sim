6d2e3cc3049cf607d7f48c8c9ebcb48de5b6ac65
Andrea Piccione <piccione@diag.uniroma1.it>
2020-02-26 15:57:57 +0100

    Added the malloc_area_get prototype to the dymelor header
    Sorry Mat, I forgot!
    
    Criticism and suggestions are always welcome!
    
    Signed-off-by: Andrea Piccione <piccione@diag.uniroma1.it>

--- src/mm/dymelor.h
+++ src/mm/dymelor.h
@@ -175,6 +175,7 @@ struct slab_chain {
  ***************/
 
 // DyMeLoR API
+extern malloc_area* malloc_area_get (void *address, int *chunk_ret);
 __attribute__((used)) void __write_mem(void *address, size_t size);
 extern size_t get_state_size(int);
 extern size_t get_log_size(malloc_state *);
