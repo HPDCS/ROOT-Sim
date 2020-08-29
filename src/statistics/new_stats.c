63bf04fb24d8a119ffab21b6dccfb3e5cda3b031
paco user <marotta@paco>
2020-08-29 12:05:14 +0200

    improved new statistics system

--- src/statistics/new_stats.c
+++ src/statistics/new_stats.c
@@ -2,7 +2,7 @@
 #include <mm/state.h>
 
 #define MACRO_PERIOD_US 1000000
-#define MICRO_PERIOD_US 100000
+#define MICRO_PERIOD_US 500000
 #define MICRO_PERIOD_DELAY 1000
 #define CLOCKS_PER_US 2200
 
@@ -20,7 +20,7 @@
 /****************
 	TMP STATS
 ****************/
-static __thread unsigned int current_executed_events 	= 0;
+static __thread unsigned int current_executed_events 		= 0;
 static __thread unsigned int current_sample_id 			= 0;
 static __thread unsigned int last_sample_id 			= 0;
 static __thread unsigned int sampling_enabled 			= 0;
@@ -29,7 +29,7 @@ static __thread unsigned int sampling_enabled 			= 0;
 	REAL STATS
 ****************/
 static __thread unsigned int sampled_rollbacks 			= 0;
-static __thread unsigned int forward_executed_events 	= 0;
+static __thread unsigned int forward_executed_events 		= 0;
 static __thread unsigned int aborted_events 			= 0;
 
 /****************
@@ -39,6 +39,7 @@ static __thread unsigned int aborted_events 			= 0;
 static __thread unsigned long long begin_time = 0;
 static __thread unsigned long long start_macro_time = 0;
 static __thread unsigned long long current_time = 0;
+static __thread bool hard = 0;
 
 typedef struct _new_stats{
 	unsigned int sampled_rollbacks;
@@ -62,56 +63,54 @@ void init_new_statistics(){
 /* scheduler.c */
 void on_process_event_forward(msg_t *evt){
 	current_executed_events	+= sampling_enabled;
-	evt->sample_id 			 = current_sample_id;
+	evt->sample_id = current_sample_id;
 }
 
 /* state.c */
 void on_log_state(state_t *log){
-	forward_executed_events 	 +=current_executed_events;
-	log->executed_events  = current_executed_events;
-	log->sample_id 		  = current_sample_id;
-	current_executed_events		  = 0;
+	forward_executed_events	+= current_executed_events;
+	log->executed_events  	 = current_executed_events;
+	log->sample_id 		 = last_sample_id;
+	current_executed_events	 = 0;
 }
 
 /* state.c (rollback) */
 void on_log_restore(){
-	sampled_rollbacks		+=sampling_enabled;
-	aborted_events			+= current_executed_events;
-	forward_executed_events	+=current_executed_events;
-	current_executed_events	 =0;
+	sampled_rollbacks	+= sampling_enabled;
+	aborted_events		+= current_executed_events;
+	forward_executed_events	+= current_executed_events;
+	current_executed_events	 = 0;
+	if(aborted_events == 0) hard = 1;
 }
 
 /* state.c (rollback) */
 void on_log_discarded(state_t *log){
-	if(log->sample_id == current_sample_id)
-		aborted_events+= log->executed_events;	
+	if((log->sample_id == last_sample_id))	aborted_events+= log->executed_events;	
 }
 
 /* state.c (silent_exec) */
 void on_process_event_silent(msg_t *evt){
-		aborted_events-=evt->sample_id == current_sample_id;
+	if(evt->sample_id == last_sample_id && last_sample_id != 0) {
+		//if(hard) printf("[MICRO STATS] AAAAAAAAAAAAAAAARGH\n");
+		aborted_events -= 1;
+	}
+
 }
 
 void OnSamplingPeriodBegin(){
-//	printf("BEGIN\n");
 	sampling_enabled=1;
-	current_sample_id = last_sample_id+1;
+	last_sample_id++;
+	current_sample_id = last_sample_id;
 	forward_executed_events = 0;
 	aborted_events = 0;
 	sampled_rollbacks = 0;
 	current_executed_events=0;
-
 }
 
 void OnSamplingPeriodEnd(){
-//	printf("END\n");
-	sampling_enabled=0;
-	last_sample_id = current_sample_id;
+	sampling_enabled= 0 ;
 	current_sample_id = 0;
-
-	forward_executed_events+=current_executed_events;
 	// TODO atomic update
-
 
 //	printf("Exec: %u, Aborted: %u, Rollbacks: %u\n", forward_executed_events, aborted_events, sampled_rollbacks);
 	__sync_fetch_and_add(&stat_collection[tid].forward_executed_events, forward_executed_events);
@@ -120,28 +119,29 @@ void OnSamplingPeriodEnd(){
 
 }
 
+void collect_statistics(){
+	if(master_thread()){  
+		double fee = 0, ae = 0, sr = 0;
+		  int i;
+		  for(i = 0; i < active_threads; i++){
+
+                	 fee += __sync_lock_test_and_set(&stat_collection[i].forward_executed_events     , 0);
+                	 ae  += __sync_lock_test_and_set(&stat_collection[i].aborted_events                      , 0);
+                	 sr  += __sync_lock_test_and_set(&stat_collection[i].sampled_rollbacks           , 0);
+   		 }
+  		 printf("[MICRO STATS] Time: %llu Exec: %f.0, ExecTh:%.2f, E[Th]:%.2f, Aborted: %f.0, PA: %.2f%, Rollbacks: %f.0, PR: %.2f%\n", current_time-begin_time,
+  		     fee, fee/((double)MICRO_PERIOD_US/1000000), (fee-ae)/((double)MICRO_PERIOD_US/1000000), ae, ae*100.0/fee, sr, sr*100.0/fee);
+	}
+	start_macro_time = current_time;
+}
+
 void process_statistics(){
 	current_time = clock_us();
 	if(start_macro_time == 0)	{begin_time = start_macro_time = current_time;}
 	
 	if(!sampling_enabled){
-		if((current_time - start_macro_time) >= MACRO_PERIOD_US){
-
-			if(master_thread()){
-				double fee = 0, ae = 0, sr = 0;
-				int i;
-				for(i = 0; i < active_threads; i++){
-
-					fee += __sync_lock_test_and_set(&stat_collection[i].forward_executed_events	, 0);
-					ae  += __sync_lock_test_and_set(&stat_collection[i].aborted_events			, 0);
-					sr  += __sync_lock_test_and_set(&stat_collection[i].sampled_rollbacks		, 0);
-				}
-				printf("Time: %llu Exec: %f.0, ExecTh:%.2f, Aborted: %f.0, PA: %.2f%, Rollbacks: %f.0, PR: %.2f%\n", current_time-begin_time,
-					fee, fee/((double)MICRO_PERIOD_US/1000000), ae, ae*100.0/fee, sr, sr*100.0/fee);
-			}
-			//	print forward_executed_events AbortedEvents
-			start_macro_time = current_time;	
-		}	
+		if((current_time - start_macro_time) >= MACRO_PERIOD_US) collect_statistics();
+		
 		if((current_time - start_macro_time) >= MICRO_PERIOD_DELAY) OnSamplingPeriodBegin();
 	}
 
